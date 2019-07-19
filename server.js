a7f34180ea5a349174859b98941b0f8c3c0f6971
sagrikaa <40899302+sagrikaa@users.noreply.github.com>
2019-07-18 23:06:15 -0400

    Backend done

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require ('express');
 const app = express();
-const PORT = process.env.PORT || 2000;
+const cors = require('cors');
+const PORT = process.env.PORT || 2001;
 const connectDB = require('./config/db');
+
+
+//Cross Platform 
+app.use(cors())
 
 //conecting to database
 connectDB();
